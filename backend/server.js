7a3d3f1c366553abca78eda4794125c3834e2737
Lalit Narayan <mjolnirxlalit@gmail.com>
2025-07-26 00:42:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const app = require('./src/app')
 require('dotenv').config()
 
+const PORT = process.env.PORT || 3000
 
-
-app.listen(3000 , ()=>{
+app.listen(PORT , ()=>{
   console.log('server started')
 })
